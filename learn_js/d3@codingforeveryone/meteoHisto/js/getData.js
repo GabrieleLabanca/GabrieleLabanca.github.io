18b5fffa6c6b51a968ca0ec384d1603201d084cb
GabrieleLabanca <gabrielelabanca@gmail.com>
2017-10-10 09:28:03 +0200

    minor

--- learn_js/d3@codingforeveryone/meteoHisto/js/getData.js
+++ learn_js/d3@codingforeveryone/meteoHisto/js/getData.js
@@ -4,9 +4,9 @@
   function makeJson(jsonp)
   {
   	mydata = jsonp.list;
-        copy_md = jsonp.list;
-        backTemp();
-        foreTemp();
+    copy_md = jsonp.list;
+    backTemp();
+    foreTemp();
   }
   var myscript = document.createElement('script');
   myscript.src = 'https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=36dc108c54b51062033b0e0d8cd81427' + '&callback=makeJson';
