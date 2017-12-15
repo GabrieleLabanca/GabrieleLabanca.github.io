3c8cd181228b97018a9340738c2f41189c8929c1
GabrieleLabanca <gabrielelabanca@gmail.com>
2017-12-16 00:39:12 +0100

    labels not working

--- js/cv_network/cv_network.js
+++ js/cv_network/cv_network.js
@@ -31,10 +31,14 @@ window.onload=function() {
       .call(d3.drag()
           .on("start", dragstarted)
           .on("drag", dragged)
-          .on("end", dragended));
-
-    node.append("title")
-      .text(function(d) { return d.id; });
+          .on("end", dragended))
+      .append("title")
+      .text(function(d) { return d.id; })
+      .append("text")
+      .attr("dx", 10)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.id; })
+      .style("stroke", "black");
 
     simulation
       .nodes(graph.nodes)
@@ -43,12 +47,27 @@ window.onload=function() {
     simulation.force("link")
       .links(graph.links);
 
+
     function ticked() {
       link
         .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
+
+      d3.selectAll("circle").attr("cx", function (d) {
+        return d.x;
+      })
+      .attr("cy", function (d) {
+        return d.y;
+      });
+
+      d3.selectAll("text").attr("x", function (d) {
+        return d.x;
+      })
+      .attr("y", function (d) {
+        return d.y;
+      });
 
       node
         .attr("cx", function(d) { return d.x; })
